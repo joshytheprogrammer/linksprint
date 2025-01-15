c881e586a13c3337981139effb7e972be6ab2541
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-01-15 22:43:29 +0100

    made changes

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Joshytheprogrammer - URL Shortener',
+  ogTitle: 'Joshytheprogrammer - URL Shortener',
+  description: 'Simplify your links effortlessly with our fast and reliable URL shortener.',
+  ogDescription: 'Simplify your links effortlessly with our fast and reliable URL shortener.',
+  ogImage: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1736970509/joshytheprogrammer/ufou176tensgw9gacdnq.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
